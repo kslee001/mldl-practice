a26bfb01e6670d2eccb779d74efff361eef22df3
kslee001 <ksnannaya@gmail.com>
2023-03-05 22:56:40 +0900

    bug fixed

--- dl-clock-recognizer/main.py
+++ dl-clock-recognizer/main.py
@@ -1,173 +1,71 @@
 from loadlibs import *
-from modules import *
-from cfg import *
+
+import modules
+import functions
+
+# ========= CONFIGURATAION ===================
+backbone = 'resnet18'
+project_name = 'clock'
+
+configs = dict()
 
 
-def prepare_data():
-    train_folder = './data/train'
-    train_data = []
-    for name in os.listdir(train_folder):
-        cur_hour = name.split("-")[0]
-        cur_min  = name.split("-")[1]
-        cur_files = glob.glob(f"{train_folder}/{name}/*.jpg")
-        for file in cur_files:
-            train_data.append([file, cur_hour, cur_min])
+configs['BATCH_SIZE'] = 128
+configs['LEARNING_RATE'] = 0.001
+configs['EPOCHS'] = 40
+configs['TEST_SIZE'] = 0.2
+configs['SEED'] = 1203
+configs['WEIGHT_DECAY'] = 0.001
+configs['DEVICE'] = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+configs['NUM_GPUS'] = torch.cuda.device_count()
+configs['TQDM'] = True
+configs['NUM_WORKERS'] = 2
 
-    train = pd.DataFrame(train_data)
-    train.columns = ['path', 'hour', 'min']
-    train['path'] = train['path'].str.replace("\\", "/", regex=False)
+configs['AUGMENTATION'] = True
+configs['SIZE'] = 128 # image size
 
-    test1_folder = './data/test1'
-    test1_data = []
-    for name in os.listdir(test1_folder):
-        cur_hour = name.split("-")[0]
-        cur_min  = name.split("-")[1]
-        cur_files = glob.glob(f"{test1_folder}/{name}/*.jpg")
-        for file in cur_files:
-            test1_data.append([file, cur_hour, cur_min])
+configs['TRAIN_FOLDER'] =  "/home/gyuseonglee/workspace/project_2days/clock/data/train"
+configs['TEST1_FOLDER'] =  "/home/gyuseonglee/workspace/project_2days/clock/data/test1"
+configs['TEST2_FOLDER'] =  "/home/gyuseonglee/workspace/project_2days/clock/data/test2"
 
-    test1 = pd.DataFrame(test1_data)
-    test1.columns = ['path', 'hour', 'min']
-    test1['path'] = test1['path'].str.replace("\\", "/", regex=False)
-
-
-    test2_folder = './data/test2'
-    test2_data = []
-    for name in os.listdir(test2_folder):
-        cur_hour = name.split("-")[0]
-        cur_min  = name.split("-")[1]
-        cur_files = glob.glob(f"{test2_folder}/{name}/*.jpg")
-        for file in cur_files:
-            test2_data.append([file, cur_hour, cur_min])
-
-    test2 = pd.DataFrame(test2_data)
-    test2.columns = ['path', 'hour', 'min']
-    test2['path'] = test2['path'].str.replace("\\", "/", regex=False)
-
-    return train, test1, test2
-
-
-def train_fn(configs, model, criterion, optimizer, scheduler, train_loader, val_loader):
-    def forward_step(batch):
-        x, y1, y2 = batch
-        x = x.to(configs['DEVICE'])
-        y1 = y1.unsqueeze(1).to(configs['DEVICE'])
-        y2 = y2.unsqueeze(1).to(configs['DEVICE'])
-
-        hours, mins = model(x)
-        loss1 = criterion(hours, y1)
-        loss2 = criterion(mins, y2)
-        loss = loss1+loss2
-        return loss, hours, mins 
-
-    best_loss = 999999
-    best_acc  = 0.0
-    best_model = None
-
-    model = model.to(configs['DEVICE'])
-    criterion = criterion.to(configs['DEVICE'])
-
-    for epoch in range(1, configs['EPOCHS']+1):
-        # train stage
-        model.train()
-        train_loss = []
-        train_iterator = tq(train_loader) if configs['TQDM'] else train_loader
-        for batch in train_iterator:
-            optimizer.zero_grad()
-            loss, _, _ = forward_step(batch)
-            loss.backward()
-            optimizer.step()
-            train_loss.append(loss.item())
-
-        if scheduler is not None:
-            scheduler.step()
-            
-        # validation stage
-        model.eval()
-        val_loss = []
-        labels = []
-        preds  = []
-        val_iterator =  tq(val_loader) if configs['TQDM'] else val_loader
-        with torch.no_grad():
-            for batch in val_loader:
-                loss, hours, mins = forward_step(batch)
-                val_loss.append(loss.item())
-                
-                # labels
-                hours = batch[1].detach.cpu().numpy().astype(str).tolist()
-                mins  = batch[2].detach.cpu().numpy().astype(str).tolist()
-                label = [ h_gt + m_gt for h_gt, m_gt in zip(hours, mins) ]
-                labels.extend(label)
-                
-                # preds
-                hours_pred = hours.argmax(1).detach().cpu().numpy().astype(str).tolist()
-                mins_pred  = mins .argmax(1).detach().cpu().numpy().astype(str).tolist()
-                pred  = [ h_pr + m_pr for h_pr, m_pr in zip(hours_pred, mins_pred) ]
-                preds.extend(pred)
-
-        # accuracy
-        acc = accuracy_score(label, pred)
-        if acc > best_acc:
-            best_acc = acc
-            best_model = model
-            
-        print(f"-- EPOCH {epoch} --")
-        print(f"training   loss : {round(np.mean(train_loss), 4)}")
-        print(f"validation loss : {round(np.mean(val_loss)  , 4)}")
-        print(f"validatoin acc  : {acc}")
-        print(f"best acc until now: {best_acc}")
+folder_name = f"./checkpoints/{backbone}_{configs['SEED']}"
+# ============================================
+           
     
-    return best_model
-                
-                
-
 def main():
-    # get data
-    train, test1, test2 = prepare_data()
-    train, val = train_test_split(train, test_size=configs['TEST_SIZE'], random_state=configs['SEED'])
-    
-    # get data loaders
-    train_loader = DataLoader(
-        BaseDataset(train['path'].values, train['hour'].values, train['min'].values, configs['SIZE'], mode='train'),
-        batch_size = configs['BATCH_SIZE'],
-        num_workers = 8,
-        pin_memory=True,
-        persistent_workers=True,
-        shuffle=True,
-    )
-    val_loader = DataLoader(
-        BaseDataset(val['path'].values, val['hour'].values, val['min'].values, configs['SIZE'], mode='val'),
-        batch_size = configs['BATCH_SIZE'],
-        num_workers = 8,
-        shuffle=False,
-    )
-    test1_loader = DataLoader(
-        BaseDataset(test1['path'].values, test1['hour'].values, test1['min'].values, configs['SIZE'], mode='test'),
-        batch_size = configs['BATCH_SIZE'],
-        num_workers = 8,
-        pin_memory=True,
-        persistent_workers=True,
-        shuffle=False,
-    )
-    test2_loader = DataLoader(
-        BaseDataset(test2['path'].values, test2['hour'].values, test2['min'].values, configs['SIZE'], mode='test'),
-        batch_size = configs['BATCH_SIZE'],
-        num_workers = 8,
-        pin_memory=True,
-        persistent_workers=True,
-        shuffle=False,
-    )
-    
+    train, test1, test2 = functions.prepare_data(configs)
+    train_loader, val_loader, test1_loader, test2_loader = functions.prepare_loaders(configs, train, test1, test2)
+
     # set training environment
-    model = ClockClassifier(model_name) # backbone
+    model = modules.ClockClassifier(backbone) # backbone
     optimizer = torch.optim.Adam(model.parameters(), lr = configs['LEARNING_RATE'])
-    criterion = torch.nn.CrossEntropyLoss()
+    criterion1 = torch.nn.CrossEntropyLoss()
+    criterion2 = torch.nn.CrossEntropyLoss()
     scheduler = None
-
-    best_model = train_fn(configs, model, criterion, optimizer, scheduler, train_loader, val_loader)    
+    best_model = functions.train_fn(configs, model, criterion1, criterion2, optimizer, scheduler, train_loader, val_loader)    
     
-    return best_model
+    # Inference
+    preds1 = functions.inference(configs, best_model, test1_loader)
+    preds2 = functions.inference(configs, best_model, test2_loader)
 
-
-if __name__ == '__main__':
-    best_model = main()
+    
+    # save outputs
+    test1['preds'] = preds1
+    test1['hour_pred'] = test1['preds'].str.split(0,2)
+    test1['min_pred']  = test1['preds'].str.split(2,4)    
+    test2['preds'] = preds2
+    test2['hour_pred'] = test2['preds'].str.split(0,2)
+    test2['min_pred']  = test2['preds'].str.split(2,4)
+    del test1['preds'], test2['preds']
+    
+    today = datetime.today()
+    today = "-".join([str(today.year), str(today.month).zfill(2), str(today.day).zfill(2), str(today.hour).zfill(2)])
+    today = today[:10].replace("-", "")
+    
+    test1.to_csv(f"output_test1_{today}.csv", encoding='utf8', index=False)
+    test2.to_csv(f"output_test2_{today}.csv", encoding='utf8', index=False)
+    
+    return 
+    
+if __name__ == "__main__":
+    preds1, labels1, preds2, labels2 = main()
